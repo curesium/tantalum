d3c87764f51f94fdc11d9ec210cf881ae01a53d0
Thijs Boom <twboom@hotmail.nl>
2020-11-10 16:49:11 +0100

    Made the first input base and second exponent for powers

--- #cobalt/js/calc.js
+++ #cobalt/js/calc.js
@@ -3,7 +3,9 @@ function calc() {
   let slots = document.querySelector('[name="slots"]');
   let characters = document.querySelector('[name="characters"]');
 
-  let count = parseInt(slots.value) + parseInt(characters.value)
+  let count = Math.pow(parseInt(slots.value), parseInt(characters.value));
+//  let count = parseInt(slots.value) + parseInt(characters.value);
+//  let count = Math.pow(5, 2)
 
   document.querySelector('#outputText').innerHTML = count
 }

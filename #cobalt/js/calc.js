007e0387bb9f4bfafa37afeaca0d216a0c5d9925
Thijs Boom <twboom@hotmail.nl>
2020-11-11 02:45:55 +0100

    Made calc.js better

--- #cobalt/js/calc.js
+++ #cobalt/js/calc.js
@@ -3,6 +3,8 @@ function calc() {
 
   // The total amount of positions available
   let positions = document.querySelector('[name="positions"]');
+  let slots = parseInt(positions.value);
+//  let positions = 10
 
   // All of the different kind of characters possible
   let lowercase = document.querySelector('[name="lowercase"]');
@@ -12,13 +14,16 @@ function calc() {
 
   // Total amount of characters
   let total = parseInt(lowercase.value) + parseInt(capital.value) + parseInt(number.value) + parseInt(characters.value)
+//  let total = 2
 
   // The formula
-  let count = Math.pow(parseInt(slots.value), parseInt(total.value));
+  let count = Math.pow(slots, total);
 
   // Display the output
   document.querySelector('#outputText').innerHTML = count
 }
+
+
 
 // For the submit button
 let submit = document.querySelector('[type="submit"]');

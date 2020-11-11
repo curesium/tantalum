0cddc57ddc223a5cc4415676142894a31b7610c0
Thijs Boom <twboom@hotmail.nl>
2020-11-11 02:50:47 +0100

    Removed unneccecary testing lines

--- #cobalt/js/calc.js
+++ #cobalt/js/calc.js
@@ -4,7 +4,6 @@ function calc() {
   // The total amount of positions available
   let positions = document.querySelector('[name="positions"]');
   let slots = parseInt(positions.value);
-//  let positions = 10
 
   // All of the different kind of characters possible
   let lowercase = document.querySelector('[name="lowercase"]');
@@ -14,7 +13,6 @@ function calc() {
 
   // Total amount of characters
   let total = parseInt(lowercase.value) + parseInt(capital.value) + parseInt(number.value) + parseInt(characters.value)
-//  let total = 2
 
   // The formula
   let count = Math.pow(slots, total);

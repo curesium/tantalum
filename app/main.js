70d52d4f9fbb4aafd05528191474236612af73e7
Thijs Boom <twboom@hotmail.nl>
2020-11-24 00:41:30 +0100

    Created basis of main.js (For actually making a window)

---
+++ app/main.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+  // Create new windows
+  mainWindow = new BrowserWindow({});
+  // Load HTML into window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Create menu template
+const mainMenuTemplate = [{
+  label: 'File'
+}
+];
